c9cc2b967eec781652362e8674afce2b61947635
Saqlain Mushtque <saqlainmushtaque@ragioneer.com>
2024-04-05 18:46:54 -0700

    Tutoria 10 completed

--- flask_blog/__init__.py
+++ flask_blog/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 from dotenv import load_dotenv
 import os
 
@@ -16,5 +17,12 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587  # Use 465 for SSL
+app.config['MAIL_USE_TLS'] = True  # Set to False if using SSL
+app.config['MAIL_USERNAME'] = os.getenv('EMAIL')
+app.config['MAIL_PASSWORD'] = os.getenv('PASSWORD')
+
+mail = Mail(app)
 
 from flask_blog import routes
